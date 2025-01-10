56a33a3d26f5df35a0d077e3eb59fecb8d2ad9f7
Nphuc2304 <hnthao2705@gmail.com>
2025-01-10 23:23:03 +0700

    form_layout_home

--- frontend/components/feature_block.tsx
+++ frontend/components/feature_block.tsx
@@ -26,8 +26,8 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   image: {
-    width: 50,
-    height: 50,
+    width: 40,
+    height: 40,
     borderRadius: 10,
   },
   text: {
