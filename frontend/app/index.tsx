d4e5fb587df8d45aecdfbead02e079387df60c39
nvhoang257 <aashoang@gmail.com>
2025-02-17 11:12:44 +0700

    api dang nhap user

--- frontend/app/index.tsx
+++ frontend/app/index.tsx
@@ -2,13 +2,14 @@ import { Text, View, StyleSheet } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
 import _layout from "./_layout";
+import Login from "./login"
 
 
 
 export default function Index() {
   return (
     <NavigationContainer>
-      <_layout />
+      <Login/>
     </NavigationContainer>
   );
 }
